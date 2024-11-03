1c042521f75acc3f4350e46e38d59517f75a5f1d
Sadi2002 <lukasz.sadowski2605@gmail.com>
2024-11-03 09:55:21 +0100

    added pricing box on mobile

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const headerDesktop = document.querySelector(".header__desktop");
+const headerMobile = document.querySelector(".header__mobile");
 
 function toggleActiveMenu() {
-  if (window.innerWidth >= 992 && window.scrollY >= 100) {
+  if (window.innerWidth >= 320 && window.scrollY >= 100) {
     headerDesktop.classList.add("active-menu");
+    headerMobile.classList.add("active-menu");
   } else {
     headerDesktop.classList.remove("active-menu");
+    headerMobile.classList.remove("active-menu");
   }
 }
 
 window.addEventListener("scroll", toggleActiveMenu);
 window.addEventListener("resize", toggleActiveMenu);
+
+toggleActiveMenu();
