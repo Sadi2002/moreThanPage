916a63c15c7f42f91ace095a3c6b6da33629c2e7
Sadi2002 <lukasz.sadowski2605@gmail.com>
2024-11-03 16:19:51 +0100

    added faq on mobile

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ window.addEventListener("scroll", toggleActiveMenu);
 window.addEventListener("resize", toggleActiveMenu);
 
 toggleActiveMenu();
+
+const faqBoxes = document.querySelectorAll(".faq__boxes__box");
+
+faqBoxes.forEach((box) => {
+  const question = box.querySelector(".faq__boxes__question");
+  const answer = box.querySelector(".faq__boxes__answear");
+
+  question.addEventListener("click", () => {
+    if (box.classList.contains("show")) {
+      answer.style.height = "0";
+      box.classList.remove("show");
+    } else {
+      // Resetujemy wszystkie inne aktywne elementy, aby tylko jedno było otwarte
+      faqBoxes.forEach((otherBox) => {
+        otherBox.classList.remove("show");
+        otherBox.querySelector(".faq__boxes__answear").style.height = "0";
+      });
+
+      // Ustawiamy wysokość elementu na jego pełną wysokość
+      answer.style.height = answer.scrollHeight + "px";
+      box.classList.add("show");
+    }
+  });
+});
